74ac8f166e5715819b072b52342bdca33a97c9e6
Tianwen Fu <tianwenf@usc.edu>
2024-12-06 14:08:18 -0800

    upload

--- python/camera_conventions/camera.py
+++ python/camera_conventions/camera.py
@@ -1,7 +1,9 @@
 import numpy as np
 from dataclasses import dataclass
 from typing import ClassVar
-from . import Convention
+import json
+from . import Convention, convention_convertion_matrix
+from copy import deepcopy
 
 
 @dataclass
@@ -12,12 +14,28 @@ class Camera:
     fy: float
     cx: float
     cy: float
-    T: np.ndarray
+    T: np.ndarray  # 4x4 matrix
     convention: Convention
 
     FLOAT_FORMAT: ClassVar[str] = "{:.10f}"
 
-    def to_txt_file(self, filename: str):
+    def convert(self, convention: Convention):
+        if self.convention == convention:
+            return self
+        matrix = convention_convertion_matrix(convention, self.convention)  # 3x3 matrix
+        C = -self.T[:3, :3].T @ self.T[:3, 3]
+        C_new = (C[np.newaxis, :] @ matrix.T).squeeze(axis=0)
+        R = self.T[:3, :3]
+        R_new = matrix @ R @ np.linalg.inv(matrix)
+        T_new = np.eye(4)
+        T_new[:3, :3] = R_new
+        T_new[:3, 3] = -R_new @ C_new
+        new_camera = deepcopy(self)
+        new_camera.T = T_new
+        new_camera.convention = convention
+        return new_camera
+
+    def to_txt(self, filename: str):
         assert self.convention == "OpenGL"
         with open(filename, "wt") as f:
             print(
@@ -34,3 +52,17 @@ class Camera:
             )
             for row in self.T:
                 print(" ".join(self.FLOAT_FORMAT.format(elem) for elem in row), file=f)
+
+    def to_json(self, filename: str):
+        data = {
+            "width": self.width,
+            "height": self.height,
+            "fx": self.fx,
+            "fy": self.fy,
+            "cx": self.cx,
+            "cy": self.cy,
+            "T": self.T.tolist(),
+            "convention": self.convention,
+        }
+        with open(filename, "wt") as f:
+            json.dump(data, f, indent=2)
